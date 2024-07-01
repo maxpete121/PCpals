4a8181c40afd5709855b1a099a9e955c78487398
Max <maxpete121@gmail.com>
2024-07-01 16:21:35 -0600

    fixes

--- client/src/env.js
+++ client/src/env.js
@@ -2,5 +2,5 @@ export const dev = window.location.origin.includes('localhost')
 export const baseURL = dev ? 'https://localhost:7045' : ''
 export const useSockets = false
 export const domain = 'dev-0mzshsqy0abngajt.us.auth0.com'
-export const clientId = 'UqJENWmOBgBlQb9arIAhk358eP9IiZcN'
+export const clientId = 'oAkS3E5lXGrQk8dX6CnhjlaZA2Lx91LQ'
 export const audience = 'https://TestApi.com'
