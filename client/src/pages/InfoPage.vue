55ca86c71133f3389d9a6fa93f6c2292e12e00e6
max <maxpete121@gmail.com>
2024-07-15 17:17:35 -0600

    fix

---
+++ client/src/pages/InfoPage.vue
@@ -0,0 +1,20 @@
+<template>
+  </template>
+  
+  <script>
+  import { computed } from 'vue';
+  import { AppState } from '../AppState';
+  export default {
+    setup() {
+      return {
+        account: computed(() => AppState.account)
+      }
+    }, components: { AdminBuildComponent }
+  }
+  </script>
+  
+  <style scoped>
+  img {
+    max-width: 100px;
+  }
+  </style>
